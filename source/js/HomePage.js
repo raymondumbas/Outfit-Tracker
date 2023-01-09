f51cb4e5f4b9ff055ea1a3451a80aecea1162029
raymondumbas <raymond.umbas@gmail.com>
2023-01-08 21:56:55 -0800

    Preview updates when user inputs an image for NCP

--- source/js/HomePage.js
+++ source/js/HomePage.js
@@ -2,6 +2,8 @@
 //Elements
 const formNCP = document.querySelector("#newClothesPrompt");
 const saveNCP = document.querySelector("#saveNCP");
+const imageNCP = document.querySelector("#imageNCP");
+const previewNCP = document.querySelector("#clothingImg")
 
 //Functions
 function submitNCP(){
@@ -17,5 +19,21 @@ function submitNCP(){
     }
 }
 
+function updatePreview() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener('load', function() {
+    previewNCP.src = reader.result;
+  });
+
+  reader.readAsDataURL(file);
+}
+
 //Event Handlers
 saveNCP.addEventListener("click",submitNCP );
+imageNCP.addEventListener('change', updatePreview);
+
+
+
+
